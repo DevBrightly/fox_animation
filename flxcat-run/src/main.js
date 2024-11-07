26fab240e61208fdce891ff51449d7e893364b08
br <itsgeorgebright@gmail.com>
2024-11-07 20:28:41 +0100

    mouth comes in open

---
+++ flxcat-run/src/main.js
@@ -0,0 +1,48 @@
+import k from "./kaplayCtx";
+import disclaimer from "./scenes/disclaimer";
+import game from "./scenes/game";
+import gameover from "./scenes/gameover";
+import mainMenu from "./scenes/mainMenu";
+
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/flxcat1.png", {
+  sliceX: 8,
+  sliceY: 2,
+  anims: {
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+k.loadSprite("ring", "graphics/coin.png", {
+  // sliceX: 16,
+  // sliceY: 1,
+  // anims: {
+  //   spin: { from: 0, to: 15, loop: true, speed: 30 },
+  sliceX: 1,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 0, loop: true, speed: 15 },
+  },
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+  sliceX: 5,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
+});
+k.loadFont("mania", "fonts/mania.ttf");
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/FELIX_THE_CAT_Opening_Theme.mp3");
+
+k.scene("disclaimer", disclaimer);
+k.scene("main-menu", mainMenu);
+k.scene("game", game);
+k.scene("gameover", gameover);
+
+k.go("disclaimer");
